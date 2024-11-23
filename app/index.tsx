916c19b8e8c0ff3b23fb8858f69bb9f855668034
Александр Печерикин <oleksandr.pecherikin@vokigames.com>
2024-11-24 00:06:11 +0200

    added NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,9 @@
 import { Text, View } from "react-native";
 import { Link } from 'expo-router'
+
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 items-center justify-center bg-blue">
       <Text>Юля Печерикина, ты хочешь знать правду о себе?</Text>
       <Link href="/skuf" style={{color:'red'}}>Нажми сюда!</Link>
     </View>
